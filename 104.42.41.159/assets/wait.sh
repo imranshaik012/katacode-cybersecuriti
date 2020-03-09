defbe0d6b1c6dad258463091b516579dd10eb0d2
Imran Shaik <33627789+imranshaik012@users.noreply.github.com>
2020-03-09 14:53:38 +0530

    senario29

--- 104.42.41.159/assets/wait.sh
+++ 104.42.41.159/assets/wait.sh
@@ -3,6 +3,15 @@
 show_progress()
 {
   echo -n "Starting"
+  echo "we are starting apisectm"
+  echo "http://104.42.41.159/swagger-ui.html "
+  echo "we are parsing OpenAPI Spec"
+  sleep 5
+  echo "Activating UNSECURED category"
+  sleep 3
+  echo "Total endpoints discovered : 75"
+  sleep 0.5
+  echo "Total Assessments written by bot : 75"
   local -r pid="${1}"
   local -r delay='0.75'
   local spinstr='\|/-'
