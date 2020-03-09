2ce8ecbe67baf25967ed6ac8b7d29a0b7e800073
Imran Shaik <33627789+imranshaik012@users.noreply.github.com>
2020-03-09 15:21:33 +0530

    senario30

--- 104.42.41.159/foreground.sh
+++ 104.42.41.159/foreground.sh
@@ -1,2 +1,18 @@
 echo "Hello..."  
-sleep 1; wait.sh
+ echo "we are starting apisectm"
+  echo "http://104.42.41.159/swagger-ui.html "
+  echo "we are parsing OpenAPI Spec"
+  sleep 5
+  echo "Activating Broken Auth category"
+  sleep 3
+  echo "Total endpoints discovered : 75"
+  sleep 0.5
+  echo "Total Assessments written by bot : 75"
+  sleep 2
+  echo "Running Broken Auth category"
+  sleep 5
+  echo "Result: 7 vulnerable endpoints detected"
+  sleep 2
+  echo "Exploitable endpoints:"
+  echo "GET:http://138.91.64.62:8080/api/v1/primary-transaction/nMBRqCPE"
+  echo "End of Scan"
